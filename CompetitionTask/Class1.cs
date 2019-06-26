75b3ae670d89ccf4644619c8f7342e90546b602d
bhushanru <bhushanru@gmail.com>
2019-06-26 12:43:33 +1200

    Update Class1.cs

--- CompetitionTask/Class1.cs
+++ CompetitionTask/Class1.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 
 namespace CompetitionTask
 {
     public class Class1
     {
+        IWebDriver driver = new ChromeDriver();
+    
     }
 }
